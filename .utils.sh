c84a89ccf4d97b3a6fe2dec598ca3e336391b6ec
Ineptus Mechanicus <ineptus.mechanicus@cartoonraccoon.art>
2023-10-12 21:11:37 +0300

    Updates

--- .utils.sh
+++ .utils.sh
@@ -56,5 +56,3 @@ export NVIM_DEVNOTES_PATH='~/nvim-plugins/devnotes.nvim'
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
-source ~/.bash-work
